15719dca9110378ab635c628d4b249b5409ba129
jokingmoony <penubokim@gmail.com>
2023-02-12 20:17:26 +0900

    Create GetWinner function

--- main.go
+++ main.go
@@ -23,6 +23,11 @@ func InitializeGame() {
 
 func GameLoop() {
 	for {
+		winner := GetWinner(board)
+		if winner != 0 {
+			fmt.Printf("Game end winner is %d\n", winner)
+			break
+		}
 		PrintGame()
 	}
 }
@@ -50,4 +55,35 @@ func ConvertToChar(num int) string {
 	} else {
 		return "X"
 	}
+}
+
+func GetWinner(board [3][3]int) int {
+
+	// horizontal match
+	i := 0
+	for i < 3 {
+		if board[i][0] == board[i][1] && board[i][0] == board[i][2] {
+			return board[i][0]
+		}
+		i++
+	}
+
+	// vertical match
+
+	j := 0
+	for j < 3 {
+		if board[0][j] == board[1][j] && board[0][j] == board[2][j] {
+			return board[0][j]
+		}
+		j++
+	}
+
+	// diagonal match
+	if board[0][0] == board[1][1] && board[0][0] == board[2][2] {
+		return board[0][0]
+	}
+	if board[0][2] == board[1][1] && board[0][2] == board[2][0] {
+		return board[0][2]
+	}
+	return 0
 }
