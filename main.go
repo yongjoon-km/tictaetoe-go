3de970c83054f571dd191024e6d930c89bdec7c4
jokingmoony <penubokim@gmail.com>
2023-02-12 20:10:11 +0900

    Create print function

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+var board [3][3]int
+
 func main() {
+	InitializeGame()
 	GameLoop()
+}
+
+func InitializeGame() {
+	i := 0
+	for i < 3 {
+		j := 0
+		for j < 3 {
+			board[i][j] = 0
+			j++
+		}
+		i++
+	}
 }
 
 func GameLoop() {
@@ -13,5 +28,26 @@ func GameLoop() {
 }
 
 func PrintGame() {
-	fmt.Println("hello game")
+	i := 0
+	for i < 3 {
+		j := 0
+		for j < 3 {
+			c := ConvertToChar(board[i][j])
+			fmt.Printf("%s|", c)
+			j++
+		}
+		fmt.Println()
+		i++
+	}
+}
+
+func ConvertToChar(num int) string {
+	if num == 0 {
+		return " "
+	}
+	if num == 1 {
+		return "O"
+	} else {
+		return "X"
+	}
 }
